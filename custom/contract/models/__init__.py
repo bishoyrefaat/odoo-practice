49bfd722d4512984f72c46ab13feb5c56ddb4b22
bishoyrefaat <bishoy91@gmail.com>
2023-07-07 08:52:05 -0700

    minor changes 3

--- custom/contract/models/__init__.py
+++ custom/contract/models/__init__.py
@@ -3,5 +3,5 @@
 from . import models
 from . import jobs
 from . import duration
-from . import holiday
+# from . import holiday
 from . import allocation
--- custom/contract/models/allocation.py
+++ custom/contract/models/allocation.py
@@ -6,5 +6,5 @@ class allocation(models.Model):
     # _name = 'contract.contract'
     # _description = 'contract.contract'
     _inherit='hr.leave.allocation'
-    contract_id = fields.Many2one( 'hr.contract')
+    contract_id = fields.Many2one( 'hr.contract',ondelete="cascade")
   
